ea2e401521678269be802fbab4d32f6ec19e3749
Sunset <vadik000056@gmail.com>
2026-08-04 17:59:07 +0500

    Initialize GreenBox I18n solution

---
+++ GreenBox.I18n.Cli/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GreenBox.I18n.Cli;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ GreenBox.I18n.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GreenBox.I18n.Core
+{
+    public class Class1
+    {
+    }
+}
